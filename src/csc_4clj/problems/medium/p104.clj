9329b5810946ae340d8299084bb9f3dc49a06fc0
Pablo González <pablog645@comunidad.unam.mx>
2019-08-23 08:52:30 -0500

    Adds the solution to problem 104.

--- src/csc_4clj/problems/medium/p104.clj
+++ src/csc_4clj/problems/medium/p104.clj
@@ -10,8 +10,24 @@
 ;; Tags: strings, math
 
 (def __
-  (fn []
-    ,,,))
+  (fn [n]
+    (loop [n        n
+           solution ""]
+      (cond
+        (>= n 1000) (recur (- n 1000) (str solution "M"))
+        (>= n 900) (recur (- n 900) (str solution "CM"))
+        (>= n 500) (recur (- n 500) (str solution "D"))
+        (>= n 400) (recur (- n 400) (str solution "CD"))
+        (>= n 100) (recur (- n 100) (str solution "C"))
+        (>= n 90) (recur (- n 90) (str solution "XC"))
+        (>= n 50) (recur (- n 50) (str solution "L"))
+        (>= n 40) (recur (- n 40) (str solution "XL"))
+        (>= n 10) (recur (- n 10) (str solution "X"))
+        (>= n 9) (recur (- n 9) (str solution "IX"))
+        (>= n 5) (recur (- n 5) (str solution "V"))
+        (>= n 4) (recur (- n 4) (str solution "IV"))
+        (>= n 1) (recur (- n 1) (str solution "I"))
+        (= n 0) solution))))
 
 
 ;;;;;;;;;;;
